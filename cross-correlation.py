6b2ee6e9096456acbaf9ce879811b8cac07fdbe6
Aditya Wardianto <adityawardianto.19073@mhs.its.ac.id>
2021-09-23 23:17:51 +0700

    Test Button

--- cross-correlation.py
+++ cross-correlation.py
@@ -30,8 +30,13 @@ class Main:
 
         st.title("Cross Correlation")
         st.caption("Aditya Wardianto 07311940000001 - Biomodelling ITS")
-        st.markdown("""<script async defer src="https://buttons.github.io/buttons.js"></script><a class="github-button" href="https://github.com/ditw11mhs/CrossCorrelation/" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Open ditw11mhs/CrossCorrelation on GitHub">Watch</a>""", unsafe_allow_html=True)
-
+        # st.markdown("""<script async defer src="https://buttons.github.io/buttons.js"></script><a class="github-button" href="https://github.com/ditw11mhs/CrossCorrelation/" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Open ditw11mhs/CrossCorrelation on GitHub">Watch</a>""", unsafe_allow_html=True)
+        streamlit.components.v1.components.html("""
+        <!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Place this tag where you want the button to render. -->
+<a class="github-button" href="https://github.com/ditw11/CrossCorrelation/subscription" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" data-show-count="true" aria-label="Watch ditw11/CrossCorrelation on GitHub">Watch</a>
+        """)
         # Time Lag
         st.header("Time Lag")
         self.t_lag = st.slider(label="Time Lag", min_value=-(self.data2_len-1), max_value=self.data2_len -
